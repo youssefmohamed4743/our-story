82d12d7b6bdfba6fa5cb452eed9e8798cc51aaa0
Youssef Mohamed <youssefmy825@gmail.com>
2026-09-16 01:44:07 +0300

    Update content.js

--- content.js
+++ content.js
@@ -340,8 +340,8 @@ export const CONTENT = {
      The website's five section titles. Change the wording if
      something else sounds more like you.                      */
   ui: {
-    storyEyebrow:    'Our',
-    storyTitle:      'Story',
+    storyEyebrow:    '❤️',
+    storyTitle:      'Our Story',
     reasonsEyebrow:  'in no particular order',
     reasonsTitle:    'Reasons I Love You',
     reasonsHint:     'tap to turn over',
